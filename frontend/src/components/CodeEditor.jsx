38320c7f59a8594f2016ba9091cf573d3a0ffa10
Suraj-3285 <suraj.mnnit85@gmail.com>
2026-03-31 21:56:46 +0530

    added code editor

---
+++ frontend/src/components/CodeEditor.jsx
@@ -0,0 +1,49 @@
+import Editor from "@monaco-editor/react";
+
+export default function CodeEditor({code,onChange}){
+    function handleMount(editor){
+        editor.focus();
+
+        setTimeout(() => {
+            editor.getAction("editor.action.formatDocument")?.run();
+        },300);
+    }
+
+    return (
+        <div
+            style = {{
+                borderRadius: "var(--radius)",
+                overflow: "hidden",
+                border: "1px solid var(--border)",
+                flexShrink: 0,
+            }}
+        >
+            <Editor 
+                height="340px"
+                language="java"
+                value={code}
+                onChange={(val) => onChange(val || "")}
+                onMount={handleMount}
+                theme="vs-dark"
+                options={{
+                    fontSize: 13,
+                    fontFamily: "'JetBrains Mono', monospace",
+                    fontLigatures: true,
+                    minimap: {enabled: false},
+                    scrollBeyondLastLine : false,
+                    lineNumbers: "on",
+                    roundedSelection: true,
+                    automaticLayout: true,
+                    tabSize: 4,
+                    wordWrap : "on",
+                    bracketPairColorization: {enabled: true},
+                    padding: {top:12, bottom: 12},
+                    scrollbar: {
+                        verticalScrollbarSize: 6,
+                        horizontalScrollbarSize: 6,
+                    },
+                }}
+            />
+        </div>
+    );
+}
